198ce3b8f4b113abe24eaf274ad34affb9c09ba4
ASIA11793 <1439083552@qq.com>
2019-11-28 08:51:37 +0800

    jj

--- app/src/main/java/com/example/recyclerviewstudentversion/MyRecyclerAdapter.java
+++ app/src/main/java/com/example/recyclerviewstudentversion/MyRecyclerAdapter.java
@@ -1,9 +1,12 @@
 package com.example.recyclerviewstudentversion;
 
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,25 +17,44 @@ import java.util.List;
 //onCreateViewHolder()
 //onBindViewHolder
 //getItemCount
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.MyViewHolder> {
     private View view;
     private List<Player> listofPlayers;
 
-    public MyAdapter(List<Player> Players1) {
-        listofPlayers = Players1;
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView name, age, worth,sport;
+        public ImageView img;
+        public MyViewHolder(View view) {
+            super(view);
+            name=view.findViewById(R.id.name);
+            age=view.findViewById(R.id.age);
+            worth=view.findViewById(R.id.money);
+            sport=view.findViewById(R.id.sport);
+            img=view.findViewById(R.id.ima);
+        }
+    }
+
+    public MyRecyclerAdapter(List<Player> Players1) {
+        this.listofPlayers = Players1;
     }
 
 
     @Override
-    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        LayoutInflater inflater = parent.getContext().getSystemService();
+    public MyRecyclerAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (view == null)
+            view = inflater.inflate(R.layout.single_item, null);
         return new MyViewHolder(view);
 
-    }
+}
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-
+        holder.name.setText("name "+listofPlayers.get(position).getName());
+        holder.age.setText("age"+listofPlayers.get(position).getage());
+        holder.worth.setText("worth"+listofPlayers.get(position).getWorth());
+        holder.sport.setText("sport"+listofPlayers.get(position).getMainsport());
+        holder.img.setImageResource(listofPlayers.get(position).getImage());
 
 
     }
@@ -44,13 +66,5 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
 
-    // Todo implement ViewHolder
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
-        // get references to each of the views in the single_item.xml
-        // Todo implement constructor
-        private MyViewHolder(@NonNull View itemView) {
-
-        }
-    }
 }
 
